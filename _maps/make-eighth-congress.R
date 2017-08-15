967c4306bd1dbfb25c15c6939cdfe403108c59ab
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-08-15 11:34:35 -0400

    Remove junk and commit sample MD maps

--- _maps/make-eighth-congress.R
+++ _maps/make-eighth-congress.R
@@ -4,7 +4,8 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(mappingelections))
 
 congress8 <- meae_maps %>% filter(congress == 8) %>% pull(meae_id)
-extra <- c("meae.congressional.congress05.md.county", 
-           "meae.congressional.congress06.md.county")
+extra <- c("meae.congressional.congress05.md.county",
+           "meae.congressional.congress06.md.county",
+           "meae.congressional.congress07.md.county")
 safe_make_page <- safely(make_map_page)
 walk(c(congress8, extra), safe_make_page, dir = ".", overwrite = TRUE)
