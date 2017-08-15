84ef4f86612fd459837b9c3afd7d0054858da3d8
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-08-15 10:50:40 -0400

    Updated tables and maps

--- _maps/make-eighth-congress.R
+++ _maps/make-eighth-congress.R
@@ -4,5 +4,7 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(mappingelections))
 
 congress8 <- meae_maps %>% filter(congress == 8) %>% pull(meae_id)
+extra <- c("meae.congressional.congress05.md.county", 
+           "meae.congressional.congress06.md.county")
 safe_make_page <- safely(make_map_page)
-walk(congress8, safe_make_page, dir = ".", overwrite = TRUE)
+walk(c(congress8, extra), safe_make_page, dir = ".", overwrite = TRUE)
